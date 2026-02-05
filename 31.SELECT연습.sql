fbb07788e0a877a941c22b868e01caf70fa633de
jjuuu <jjuuu2727@gmail.com>
2026-02-05 15:51:25 +0900

    7th

--- 31.SELECT연습.sql
+++ 31.SELECT연습.sql
@@ -607,6 +607,7 @@ ON 사원.부서번호 = 부서.부서번호;
 SELECT *
 FROM 사원 LEFT JOIN 부서
 ON 사원.부서번호 = 부서.부서번호;
+# WHERE 부서.부서번호 IS NULL >부서번호 없는 사원만 보여주려면,, 근데 사원.부서번호는 NULL값이 아닌 공백값이기 때문에 부서.부서번호 IS NULL로 조건 해야 함.
 
 SELECT *
 FROM 사원 RIGHT JOIN 부서
@@ -615,4 +616,179 @@ ON 사원.부서번호 = 부서.부서번호;
 SELECT *
 FROM 고객 LEFT JOIN 주문
 ON 고객.고객번호 = 주문.고객번호
-WHERE 주문번호 IS NULL;
+WHERE 주문번호 IS NULL;사원번호
+
+USE WNTRADE;
+
+SELECT  A.고객회사명, A.담당자명, A.마일리지
+FROM 고객 AS A
+LEFT JOIN 고객 AS B
+ON A.마일리지 < B.마일리지
+WHERE B.고객번호 IS NULL;
+
+SELECT 고객번호
+, 고객회사명
+, 담당자명
+, 마일리지
+, 등급명
+FROM 고객 JOIN 마일리지등급
+ON 마일리지 BETWEEN 하한마일리지 AND 상한마일리지
+WHERE 담당자명 = '이은광';
+
+#사원번호, 사원의 이름, 상사의 사원번호, 상사의 이름을
+SELECT 사원.사원번호
+, 사원.이름
+, 상사.사원번호 AS '상사 사원번호'
+, 상사.이름 AS '상사 이름'
+FROM 사원 JOIN 사원 AS 상사 
+ON 사원.상사번호 = 상사.사원번호;
+
+#상사가 없는 사원만
+SELECT 사원.사원번호
+, 사원.이름
+, 상사.사원번호 AS '상사 사원번호'
+, 상사.이름 AS '상사 이름'
+FROM 사원 LEFT JOIN 사원 AS 상사 
+ON 사원.상사번호 = 상사.사원번호
+WHERE 사원.상사번호 = '';
+
+#서브쿼리 >> 이렇게 서브쿼리를 사용하는 이유가 이게 좀 더 직관적임. 비교(앞에 AS A AS B 이거 두개)
+SELECT 고객번호
+, 고객회사명
+, 담당자명
+, 마일리지
+FROM 고객
+WHERE 마일리지 = (SELECT MAX(마일리지)
+				FROM 고객
+                );
+
+
+#주문번호 ‘H0250’을 주문한 고객에 대해 고객회사명과 담당자명을 보이시오.
+SELECT 고객회사명
+, 담당자명
+FROM 고객 JOIN 주문
+ON 고객.고객번호 = 주문.고객번호
+WHERE 주문번호 = 'H0250';
+
+SELECT 고객회사명
+, 담당자명
+FROM 고객
+WHERE 고객.고객번호 = ( SELECT 주문.고객번호
+						FROM 주문
+						WHERE 주문.주문번호 = 'H0250'
+                        );
+                        
+SELECT *
+FROM 고객
+WHERE 고객.고객번호 = (SELECT 주문.고객번호
+						FROM 주문
+                        WHERE 주문.주문번호 = 'H0250');
+                        
+# ‘부산광역시’고객의 최소 마일리지보다 더 큰 마일리지를 가진 고객 정보를 보이시오
+SELECT *
+FROM 고객
+WHERE 마일리지 > (SELECT MIN(마일리지)
+					FROM 고객
+					WHERE 도시 = '부산광역시'
+					);
+
+#‘부산광역시’ 고객이 주문한 주문건수를 보이시오.
+SELECT COUNT(*) AS 주문건수
+FROM 고객
+WHERE 고객번호 IN (SELECT 고객번호
+					FROM 고객
+					WHERE 도시 = '부산광역시'
+);
+
+SELECT COUNT(*) AS '주문건수'
+FROM 고객
+WHERE 고객번호 IN (SELECT 고객번호
+				FROM 고객
+				WHERE 도시 LIKE '부산%'
+);
+
+# 부산광역시’ 고객이 주문한 주문건수를 보이시오.
+SELECT COUNT(*) AS 주문건수
+FROM 주문
+WHERE 고객번호 IN (SELECT 고객번호
+					FROM 고객
+					WHERE 도시 LIKE '부산%'
+);
+
+# 부산광역시’ 전체 고객의 마일리지보다 마일리지가 큰 고객의 정보를 보이시오.
+SELECT *
+FROM 고객
+WHERE 마일리지 > (SELECT SUM(마일리지)
+					FROM 고객
+					WHERE 도시 LIKE '부산%'
+);
+
+#ANY / ALL /EXIST
+SELECT *
+FROM 고객
+WHERE 마일리지 > ANY (SELECT 마일리지
+					FROM 고객
+					WHERE 도시 LIKE '부산%'
+);
+
+SELECT *
+FROM 고객
+WHERE 마일리지 > ALL (SELECT 마일리지
+					FROM 고객
+					WHERE 도시 LIKE '부산%'
+);
+
+
+# 각 지역의 어느 평균 마일리지보다도 마일리지가 큰 고객의 정보를 보이시오.
+SELECT *
+FROM 고객
+WHERE 마일리지 > ANY ;
+
+SELECT *
+FROM 고객 AS A
+WHERE EXISTS (SELECT 1
+				FROM 고객 AS B
+				WHERE 도시 = '부산광역시' AND A.마일리지 > B.마일리지
+);
+
+/* 고객 전체의 평균마일리지보다 평균마일리지가 큰 도시에 대해 
+ 도시명과 도시의 평균마일리지를 보이시오. */
+SELECT 도시
+, AVG(마일리지) AS '도시평균마일리지'
+, (
+						SELECT AVG(마일리지)
+						FROM 고객
+) AS 전체평균마일리지
+FROM 고객
+GROUP BY 도시
+HAVING AVG(마일리지) > (
+						SELECT AVG(마일리지)
+						FROM 고객
+);
+/* 담당자명, 고객회사명, 마일리지, 도시, 해당 도시의 평균마일리지를 보이시오. 
+그리고 고객이 위치하는 도시의 평균마일리지와 각 고객의 마일리지 간의 차이도 
+함께 보이시오. */
+SELECT 담당자명
+, 고객회사명
+, 마일리지
+, 고객.도시
+, 도시평균마일리지
+, 도시평균마일리지 - 마일리지 AS '마일리지 간 차이'
+FROM 고객
+, (
+SELECT 도시
+, AVG(마일리지) AS '도시평균마일리지'
+FROM 고객 
+GROUP BY 도시 
+) AS 도시별요약
+WHERE 고객.도시 = 도시별요약.도시;
+
+#고객번호, 담당자명과 고객의 최종 주문일을 보이시오
+SELECT 고객번호
+, 담당자명
+, (SELECT MAX(주문일)
+FROM 주문
+WHERE 주문.고객번호 = 고객.주문번호
+) AS 최종주문일
+FROM 고객;
+
